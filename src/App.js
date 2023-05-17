c2f3e45b0852d8bff4e44383cc68fad886eea8a7
ARIEL DAN COHEN <46914677@ort>
2023-05-17 09:06:26 -0300

    Falta form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 import Card from './Card';
 
 function App() {
   return (
-    <ul className='lista-citas'>
-      <Card mascota="Nina" duenio="Martin" fecha="2021-08-05" hora="08:20" sintomas="Le duele la pierna"/>
-      <Card mascota="Sifon" duenio="Flecha" fecha="2023-08-05" hora="09:24" sintomas="Duerme mucho"/>
-      <Card mascota="Floki" duenio="Ari" fecha="2023-08-05" hora="16:15" sintomas="No está comiendo"/>
-    </ul>
+    <>
+      <h1>ADMINISTRADOR DE PACIENTES</h1>
+      <div className='one-half column'>
+        <h2>ADMINISTRA TUS CITAS</h2>
+        <Card mascota="Nina" duenio="Martin" fecha="2021-08-05" hora="08:20" sintomas="Le duele la pierna" />
+        <Card mascota="Sifon" duenio="Flecha" fecha="2023-08-05" hora="09:24" sintomas="Duerme mucho" />
+        <Card mascota="Floki" duenio="Ari" fecha="2023-08-05" hora="16:15" sintomas="No está comiendo" />
+      </div>
+    </>
   );
 }
 
